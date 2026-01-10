417da8cfe8819b44593fc697ca34cef40bb8c653
cobungra <bernard@lecity.edu.au>
2026-01-11 09:51:09 +1100

    Colour

--- pocket.py
+++ pocket.py
@@ -27,7 +27,7 @@ COLORS = [
     ("yellow", (1, 0.3, 0), "yellow.img","QYT_KT-WP12"),
     ("blue",   (0, 0.1, 1), "blue.img","QYT_KT-WP12"),
     ("red",    (1, 0, 0), "red.img","QYT_KT-WP12"),
-    ("pink",   (1, 0.1, 1), "pink.img","Baofeng_UV-5R"),
+    ("pink",   (1, 0.1, 0.1), "pink.img","Baofeng_UV-5R"),
     ("cyan",   (0, 1, 1), "cyan.img","Baofeng_UV-5R"),
     ("purple", (0.3, 0, 1), "purple.img","Baofeng_UV-5R"),
 ]
